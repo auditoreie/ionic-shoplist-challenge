6313823ae430715910b60c690489c590926f14b0
arthurrguimaraes12 <arthurrguimaraes12@gmail.com>
2020-02-23 09:26:58 -0300

    teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+  { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)},
+  { path: 'lista', loadChildren: () => import('./pages/lista/lista.module').then( m => m.ListaPageModule)},
+ 
+
+
+
 ];
 
 @NgModule({
